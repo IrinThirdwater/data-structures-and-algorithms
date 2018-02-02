1d73403baec304d9d4ed3e30dda615c13b107334
Jirapat Treyanurak <34905030+IrinThirdwater@users.noreply.github.com>
2018-02-02 06:53:36 +0000

    Update SortTest.java
    Switch to @ParameterizedTest introduced in JUnit Jupiter

--- SortTest.java
+++ SortTest.java
@@ -1,36 +1,47 @@
 import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.*;
+import org.junit.jupiter.params.provider.*;
+
 
 class SortTest {
 
-    private static int[] expected0;
-    private static int[] expected1;
-    private static int[] expected2;
-    private static int[] expectedSorted;
-    private static int[] expectedReverse;
-    private static int[] expectedRandom;
-    private static int[] expectedRandomBig;
-    private static int[] expectedConstant;
-    private static int[] expectedDuplicates;
-
-    private int[] array0;
-    private int[] array1;
-    private int[] array2;
-    private int[] arraySorted;
-    private int[] arrayReverse;
-    private int[] arrayRandom;
-    private int[] arrayRandomBig;
-    private int[] arrayConstant;
-    private int[] arrayDuplicates;
-
-    @BeforeAll static void setExpectedArrays () {
-        expected0 = new int[0];
-        expected1 = new int[]{8};
-        expected2 = new int[]{2,8};
-        expectedSorted = new int[]{-5,-1,0,2,7,26571};
-        expectedReverse = new int[]{-1,3,6,7,9};
-        expectedRandom = new int[]{-512,-256,2,6,11,457,512,75146};
-        expectedRandomBig = new int[]{
+    /**
+     * ArrayCase describe different test cases to be tested on sorting methods.
+     */
+    private enum ArrayCase {
+        ZERO(
+            "0",        // name
+            new int[0], // sortedArray
+            new int[0]  // testArray
+        ),
+        ONE(
+            "1",
+            new int[]{8},
+            new int[]{8}
+        ),
+        TWO(
+            "2",
+            new int[]{2,8},
+            new int[]{8,2}
+        ),
+        SORTED(
+            "Sorted",
+            new int[]{-5,-1,0,2,7,26571},
+            new int[]{-5,-1,0,2,7,26571}
+        ),
+        REVERSE(
+            "Reverse-sorted",
+            new int[]{-1,3,6,7,9},
+            new int[]{9,7,6,3,-1}
+        ),
+        RANDOM(
+            "Random (small)",
+            new int[]{-512,-256,2,6,11,457,512,75146},
+            new int[]{457,2,75146,-512,512,6,11,-256}
+        ),
+        RANDOM_BIG(
+            "Random (big)",
+            new int[]{
                 -1270152,-74521,-43122,-14325,-13252,-11445,
                 -445,-123,-51,-51,-44,-32,-31,-13,1,3,41,43,
                 89,117,126,131,456,462,645,1365,2138,4261,
@@ -40,18 +51,8 @@ class SortTest {
                 61322,64512,76813,77154,77929,102342,221232,
                 270512,291139,462351,642512,645312,700921,
                 785612,1465990,4125122,7812588,43270320
-        };
-        expectedConstant = new int[]{8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8};
-        expectedDuplicates = new int[]{-1,-1,0,0,0,2,2,2,3,4,4,4,7,7,7,9,9};
-    }
-    @BeforeEach void setTestArrays () {
-        array0 = new int[0];
-        array1 = new int[]{8};
-        array2 = new int[]{8,2};
-        arraySorted = new int[]{-5,-1,0,2,7,26571};
-        arrayReverse = new int[]{9,7,6,3,-1};
-        arrayRandom = new int[]{457,2,75146,-512,512,6,11,-256};
-        arrayRandomBig = new int[]{
+            },
+            new int[]{
                 456,117,43270320,-32,35744,131,462,20917,
                 126,89,-74521,-44,3,77154,21050,21643,-51,
                 21634,7812588,44231,-1270152,64512,785612,
@@ -61,65 +62,58 @@ class SortTest {
                 2138,42130,-13252,-43122,47561,61322,6674,
                 41322,645312,61234,7751,41,1365,-14325,-31,
                 21342,46754,221232,22555,-13,-445,1465990,1
-        };
-        arrayConstant = new int[]{8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8};
-        arrayDuplicates = new int[]{4,9,4,-1,9,0,2,2,2,7,-1,0,3,4,7,0,7};
+            }
+        ),
+        CONSTANT(
+            "Constant",
+            new int[]{8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8},
+            new int[]{8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8}
+        ),
+        DUPLICATES(
+            "Duplicates",
+            new int[]{-1,-1,0,0,0,2,2,2,3,4,4,4,7,7,7,9,9},
+            new int[]{4,9,4,-1,9,0,2,2,2,7,-1,0,3,4,7,0,7}
+        );
+
+        private final String name;
+        private final int[] sortedArray;
+        private final int[] testArray;
+        // ArrayCase Constructor
+        ArrayCase (String name, int[] sortedArray, int[] testArray) {
+            this.name = name;
+            this.sortedArray = sortedArray;
+            this.testArray = testArray;
+        }
+        // Accessors
+        public int[] expectedArray () {
+            return this.sortedArray;
+        }
+        public int[] testArray () {
+            return this.testArray;
+        }
+        @Override public String toString () {
+            return this.name;
+        }
     }
 
-    @Test void insertionSort0 () {
-        assertArrayEquals(expected0, Sort.insertionSort(array0));
+    @ParameterizedTest
+    @EnumSource(ArrayCase.class)
+    void insertionSort (ArrayCase testCase) {
+        assertArrayEquals(
+            testCase.expectedArray(),
+            Sort.insertionSort(testCase.testArray()),
+            testCase.toString()
+        );
     }
-    @Test void insertionSort1 () {
-        assertArrayEquals(expected1, Sort.insertionSort(array1));
-    }
-    @Test void insertionSort2 () {
-        assertArrayEquals(expected2, Sort.insertionSort(array2));
-    }
-    @Test void insertionSortSorted () {
-        assertArrayEquals(expectedSorted, Sort.insertionSort(arraySorted));
-    }
-    @Test void insertionSortReverse () {
-        assertArrayEquals(expectedReverse, Sort.insertionSort(arrayReverse));
-    }
-    @Test void insertionSortRandom () {
-        assertArrayEquals(expectedRandom, Sort.insertionSort(arrayRandom));
-    }
-    @Test void insertionSortRandomBig () {
-        assertArrayEquals(expectedRandomBig, Sort.insertionSort(arrayRandomBig));
-    }
-    @Test void insertionSortConstant () {
-        assertArrayEquals(expectedConstant, Sort.insertionSort(arrayConstant));
-    }
-    @Test void insertionSortDuplicates () {
-        assertArrayEquals(expectedDuplicates, Sort.insertionSort(arrayDuplicates));
-    }
-
-    @Test void selectionSort0 () {
-        assertArrayEquals(expected0, Sort.selectionSort(array0));
-    }
-    @Test void selectionSort1 () {
-        assertArrayEquals(expected1, Sort.selectionSort(array1));
-    }
-    @Test void selectionSort2 () {
-        assertArrayEquals(expected2, Sort.selectionSort(array2));
-    }
-    @Test void selectionSortSorted () {
-        assertArrayEquals(expectedSorted, Sort.selectionSort(arraySorted));
-    }
-    @Test void selectionSortReverse () {
-        assertArrayEquals(expectedReverse, Sort.selectionSort(arrayReverse));
-    }
-    @Test void selectionSortRandom () {
-        assertArrayEquals(expectedRandom, Sort.selectionSort(arrayRandom));
-    }
-    @Test void selectionSortRandomBig () {
-        assertArrayEquals(expectedRandomBig, Sort.selectionSort(arrayRandomBig));
-    }
-    @Test void selectionSortConstant () {
-        assertArrayEquals(expectedConstant, Sort.selectionSort(arrayConstant));
-    }
-    @Test void selectionSortDuplicates () {
-        assertArrayEquals(expectedDuplicates, Sort.selectionSort(arrayDuplicates));
+    
+    @ParameterizedTest
+    @EnumSource(ArrayCase.class)
+    void selectionSort (ArrayCase testCase) {
+        assertArrayEquals(
+            testCase.expectedArray(),
+            Sort.selectionSort(testCase.testArray()),
+            testCase.toString()
+        );
     }
 
     private String arrayToString (int[] array) {
